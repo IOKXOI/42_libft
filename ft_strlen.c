c11c33e04aab787ac18b8597f366f0b1bb71e2c7
IOK <sydauria@student.42.fr>
2024-02-02 14:57:23 +0100

    fix strlen

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: sydauria <sydauria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 06:57:35 by sydauria          #+#    #+#             */
-/*   Updated: 2024/02/02 13:15:02 by sydauria         ###   ########.fr       */
+/*   Updated: 2024/02/02 14:56:59 by sydauria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ size_t	ft_strlen(const char *str)
 	size_t	i;
 
 	if (!str || !*str)
-		return (NULL);
+		return (0);
 	i = 0;
 	while (str[i])
 		i++;
